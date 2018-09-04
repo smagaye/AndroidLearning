5b3c4da2a29c1e54f0c1294049b33d8e1d53afa7
smag <sma.gaye94@gmail.com>
2018-09-04 16:16:17 +0000

    Initialisation du fichier Driver

--- app/src/main/java/com/smag/androidlearning/Driver.java
+++ app/src/main/java/com/smag/androidlearning/Driver.java
@@ -23,64 +23,10 @@ import java.util.Date;
 public class Driver extends AppCompatActivity {
 
     DatabaseFactory databaseFactory;
-
     private static String MsgLog ="MsgConsole";
 
-   /* public static void main(String[] args) {
-        Document doc= XmlManager.getDocument("/Users/apple/AndroidStudioProjects/androidLearning/app/src/main/res/xml/androidlearningdata.xml");
-        XmlManager.runXmlFile(doc);
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
-   */
-    public static void daoTest(){
-         /*databaseFactory = DatabaseFactory.getAppDatabase(getApplicationContext());
-
-        //Persistence Ressourcedescription
-        Ressourcedescription ressourcedescription = new Ressourcedescription();
-        ressourcedescription.setTitre("Layout");
-        ressourcedescription.setDescription("Les vues sur android");
-        ressourcedescription.setEtat("non lu");
-        ressourcedescription.setPhoto("");
-        databaseFactory.getRessourcedescriptionDao().persist(ressourcedescription);
-
-        Log.i(MsgLog,"Titre Ressource " + databaseFactory.getRessourcedescriptionDao().getAllRessourcedescriptions().get(0).toString());
-
-        //Persistence Theme
-        Theme theme = new Theme();
-        theme.setRessourcedescription(ressourcedescription);
-        theme.setDateactivation(new Date());
-        theme.setTitre("Theme Widget");
-        databaseFactory.getThemeDao().persist(theme);
-
-        Log.i(MsgLog,"Date activation Theme: " + databaseFactory.getThemeDao().getAllThemes().get(0).toString());
-
-        // Persistence Cours
-        Cours cours = new Cours();
-        Ressourcedescription ressourcedescription1 = new Ressourcedescription();
-        ressourcedescription1.setTitre("LinearLayout");
-        ressourcedescription1.setDescription("Dispositon des elements");
-        ressourcedescription1.setEtat("lu");
-        ressourcedescription1.setPhoto("pop.png");
-
-        cours.setTheme(theme);
-        cours.setRessourcedescription(ressourcedescription1);
-        cours.setDatedernierelecture(new Date());
-        databaseFactory.getCoursDao().persist(cours);
-
-        Log.i(MsgLog,"Identifiant Cours: " + databaseFactory.getCoursDao().getAllCours().get(0).toString());
-
-
-        //Persistence Exercice
-        Exercice exercice =new Exercice();
-        exercice.setScore(10);
-        exercice.setTheme(theme);
-        exercice.setSequencequestion("1-3;2-7");
-        exercice.setTempsreponse(27);
-
-        databaseFactory.getExerciceDao().persist(exercice);
-
-        Log.i(MsgLog,"\nExercice : " + databaseFactory.getExerciceDao().getAllExercices().get(0).toString());
-    */
-
-    }
-
 }
