ea6f3bfb9f58d5bc2fb06a024969c986eea46f27
smag <sma.gaye94@gmail.com>
2018-09-12 18:28:06 +0000

    Mise en place du theme de l'application

--- app/src/main/java/com/smag/androidlearning/AppViewContainer.java
+++ app/src/main/java/com/smag/androidlearning/AppViewContainer.java
@@ -1,13 +1,192 @@
 package com.smag.androidlearning;
 
+import android.graphics.Color;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import devlight.io.library.ntb.NavigationTabBar;
 
 public class AppViewContainer extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_view_container);
+        initUI();
+    }
+
+    private void initUI() {
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.vp_horizontal_ntb);
+        viewPager.setAdapter(new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return 5;
+            }
+
+            @Override
+            public boolean isViewFromObject(final View view, final Object object) {
+                return view.equals(object);
+            }
+
+            @Override
+            public void destroyItem(final View container, final int position, final Object object) {
+                ((ViewPager) container).removeView((View) object);
+            }
+
+            @Override
+            public Object instantiateItem(final ViewGroup container, final int position) {
+                final View view = LayoutInflater.from(
+                        getBaseContext()).inflate(R.layout.item_vp_list, null, false);
+
+                final RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.rv);
+                recyclerView.setHasFixedSize(true);
+                recyclerView.setLayoutManager(new LinearLayoutManager(
+                                getBaseContext(), LinearLayoutManager.VERTICAL, false
+                        )
+                );
+                recyclerView.setAdapter(new RecycleAdapter());
+
+                container.addView(view);
+                return view;
+            }
+        });
+
+        final String[] colors = getResources().getStringArray(R.array.default_preview);
+
+        final NavigationTabBar navigationTabBar = (NavigationTabBar) findViewById(R.id.ntb_horizontal);
+        final ArrayList<NavigationTabBar.Model> models = new ArrayList<>();
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_home_black_24dp),
+                        Color.parseColor(colors[0]))
+                        .title("Home")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_account_box_black_24dp),
+                        Color.parseColor(colors[1]))
+                        .title("User")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_settings_black_24dp),
+                        Color.parseColor(colors[2]))
+                        .title("Settings")
+                        .build()
+        );
+
+        navigationTabBar.setModels(models);
+        navigationTabBar.setViewPager(viewPager, 1);
+
+        //IMPORTANT: ENABLE SCROLL BEHAVIOUR IN COORDINATOR LAYOUT
+        navigationTabBar.setBehaviorEnabled(true);
+
+        navigationTabBar.setOnTabBarSelectedIndexListener(new NavigationTabBar.OnTabBarSelectedIndexListener() {
+            @Override
+            public void onStartTabSelected(final NavigationTabBar.Model model, final int index) {
+            }
+
+            @Override
+            public void onEndTabSelected(final NavigationTabBar.Model model, final int index) {
+                model.hideBadge();
+            }
+        });
+        navigationTabBar.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(final int position, final float positionOffset, final int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(final int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(final int state) {
+
+            }
+        });
+
+        final CoordinatorLayout coordinatorLayout = (CoordinatorLayout) findViewById(R.id.parent);
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                for (int i = 0; i < navigationTabBar.getModels().size(); i++) {
+                    final NavigationTabBar.Model model = navigationTabBar.getModels().get(i);
+                    navigationTabBar.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            final String title = String.valueOf(new Random().nextInt(15));
+                            if (!model.isBadgeShowed()) {
+                                model.setBadgeTitle(title);
+                                model.showBadge();
+                            } else model.updateBadgeTitle(title);
+                        }
+                    }, i * 100);
+                }
+
+                coordinatorLayout.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        final Snackbar snackbar = Snackbar.make(navigationTabBar, "Notification", Snackbar.LENGTH_SHORT);
+                        snackbar.getView().setBackgroundColor(Color.parseColor("#38D0FD"));
+                        ((TextView) snackbar.getView().findViewById(R.id.snackbar_text))
+                                .setTextColor(Color.parseColor("#1164F9"));
+                        snackbar.show();
+                    }
+                }, 1000);
+            }
+        });
+
+        final CollapsingToolbarLayout collapsingToolbarLayout =
+                (CollapsingToolbarLayout) findViewById(R.id.toolbar);
+        collapsingToolbarLayout.setExpandedTitleColor(Color.parseColor("#38D0FD"));
+        collapsingToolbarLayout.setCollapsedTitleTextColor(Color.parseColor("#ffffff"));
+    }
+
+    public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.ViewHolder> {
+
+        @Override
+        public ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
+            final View view = LayoutInflater.from(getBaseContext()).inflate(R.layout.theme_item, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, final int position) {
+            //holder.txt.setText(String.format("Navigation Item #%d", position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return 5;
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+
+            public TextView txt;
+
+            public ViewHolder(final View itemView) {
+                super(itemView);
+                //txt = (TextView) itemView.findViewById(R.id.txt_vp_item_list);
+            }
+        }
     }
 }
